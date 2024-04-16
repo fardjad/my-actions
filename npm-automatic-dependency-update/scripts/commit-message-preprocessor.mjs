4369ee0344227aeb53bde0df9fca46416e369496
Fardjad Davari <public@fardjad.com>
2024-04-16 12:20:56 +0200

    commit-message-preprocessor: add support for deep and shallow ncu output

--- npm-automatic-dependency-update/scripts/commit-message-preprocessor.mjs
+++ npm-automatic-dependency-update/scripts/commit-message-preprocessor.mjs
@@ -2,33 +2,73 @@
 
 import readline from "node:readline";
 
-const printVersions = (data) => {
-  for (const [name, version] of Object.entries(data)) {
-    console.log(`${name}: ${version}`);
+const createPromiseWithResolvers = () => {
+  let resolve;
+  let reject;
+
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return { promise, resolve, reject };
+};
+
+const readLinesFromStdin = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false, // This ensures readline doesn't treat input as a terminal
+  });
+
+  const lines = [];
+
+  rl.on("line", (line) => {
+    lines.push(line);
+  });
+
+  const { promise: waitForStdin, resolve } = createPromiseWithResolvers();
+
+  rl.on("close", () => {
+    resolve(lines);
+  });
+
+  await waitForStdin;
+
+  return lines;
+};
+
+const isDeep = (ncuOutput) => {
+  if (Object.values(ncuOutput).some((value) => typeof value === "object")) {
+    return true;
   }
 };
 
-let lineNumber = 0;
+const printVersions = (data) => {
+  let deps = data;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false, // This ensures readline doesn't treat input as a terminal
-});
-
-rl.on("line", (line) => {
-  lineNumber += 1;
-
-  if (lineNumber < 3) {
-    console.log(line);
-    return;
+  if (isDeep(data)) {
+    deps = Object.values(data).reduce((acc, obj) => ({ ...acc, ...obj }), {});
   }
 
-  const newLine = line.replace(/^[^{]+/, "").trim();
-
-  if (newLine === "") {
-    return;
+  const output = [];
+  for (const [key, value] of Object.entries(deps)) {
+    output.push(`${key}: ${value}`);
   }
 
-  printVersions(JSON.parse(newLine));
-});
+  return output.join("\n");
+};
+
+const splitCommitMessage = (commitMessageLines) => {
+  const title = commitMessageLines[0];
+  const body = commitMessageLines.slice(1).join("\n");
+
+  return { title, body };
+};
+
+const stdinLines = await readLinesFromStdin();
+const { title, body } = splitCommitMessage(stdinLines);
+
+console.log(title);
+const ncuOutput = JSON.parse(body);
+console.log(`\n${printVersions(ncuOutput)}`);
